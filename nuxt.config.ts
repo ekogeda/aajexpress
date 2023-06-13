9a05e41df87da220299b64aaae5f148e472f28da
Demola <64336052+ekogeda@users.noreply.github.com>
2023-06-13 22:28:40 +0100

    Updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,4 @@
 export default defineNuxtConfig({
-	app: {
-		pageTransition: {
-			name: 'fade',
-			mode: 'out-in', // default
-		},
-		layoutTransition: {
-			name: 'slide',
-			mode: 'out-in', // default
-		},
-		head: {
-			titleTemplate: '%s - aaj',
-			meta: [
-				{ charset: 'utf-8' },
-				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-				{ hid: 'description', name: 'description', content: '' },
-				{ name: 'format-detection', content: 'telephone=no' },
-			],
-			link: [
-				{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-				{
-					rel: 'stylesheet',
-					href: 'https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap',
-				},
-			],
-		},
-	},
 	alias: {
 		assets: '/<rootDir>/assets',
 	},
